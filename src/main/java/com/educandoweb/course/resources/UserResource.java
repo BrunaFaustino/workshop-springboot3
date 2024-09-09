827cf5501ddf305ab75d8dfc26a76d174f1c9b29
Bruna Faustino <faustibruna@hotmail.com>
2024-09-09 21:50:50 +0100

    Database populada, resource acessando database

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,18 +1,27 @@
 package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    private final UserRepository userRepository;
+
+    public UserResource(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(15L, "Maria", "maria@gmail.com", "123", "97784558");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> result = userRepository.findAll();
+        return ResponseEntity.ok().body(result);
     }
 }
